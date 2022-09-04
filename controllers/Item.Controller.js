641a10fc62692a9ace29a5a872663b61d7cc9b9e
Kev <kevin@mobilityps.com>
2022-09-04 05:51:42 -0300

    ¨manejo la data como lo pide en el pdf

--- controllers/Item.Controller.js
+++ controllers/Item.Controller.js
@@ -3,11 +3,40 @@ const itemService  = require("../services/Item.Service");
 
 const searchItems = async (query) => {
    const items = await itemService.searchItems(query); 
-   return items;
+   let response = filterSearchResults(items)
+   return response;
 };
 const getDetailsById = async (id) => {
    let items = await itemService.getDetailsById(id); 
-   return items;
+   let response = filterDetailsResponse(items);
+   return response;
 };
+
+function filterDetailsResponse(data){
+
+   let response ={"author": {
+      "name": "Kevin",
+      "lastname": "Lewicki"
+    },
+  }
+  response.item = {...data[0], ...data[1]}
+  return response;
+}
+
+function filterSearchResults(data){
+   let response =
+   {
+     "author": {
+       "name": "Kevin",
+       "lastname": "Lewicki"
+     },
+   }
+ 
+   let categorias = data.filters[0]?.values[0]?.path_from_root?.map(e => e.name);
+   response.categories = categorias
+   response.items = data.results
+   return response
+}
+
 
 module.exports = { searchItems ,getDetailsById};
